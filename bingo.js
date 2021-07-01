36a510a84ee63702e585d588cdb85bcc487c5343
barrytrearty <btrearty@gmail.com>
2021-07-01 17:04:55 +0100

    refactored the unique number function

--- bingo.js
+++ bingo.js
@@ -12,7 +12,6 @@ const numberOfPlayers = document.getElementById("number-of-players");
 let alreadySelected = [];
 
 // Create new game
-
 const generateGameBoard = function () {
   playButton.remove();
   title.innerText = "LETS PLAY";
@@ -20,8 +19,6 @@ const generateGameBoard = function () {
     let newSquare = document.createElement("div");
     newSquare.innerText = number;
     newSquare.classList.add("number-square");
-    // newSquare.classList.add("selected");
-    // newSquare.onclick = selectDay;
     gameBoard.appendChild(newSquare);
   }
   boardButton.classList.remove("hidden");
@@ -29,51 +26,44 @@ const generateGameBoard = function () {
   gameBoard.classList.remove("hidden");
 };
 
-// Add new player card
-
-const generatePlayerBoard = function () {
-  let newPlayer = document.createElement("div");
-  newPlayer.classList.add("new-player");
-  userSection.appendChild(newPlayer);
-  //   newPlayer
-
-  let playerNumberArray = [];
-  for (let num = 1; num <= 24; num++) {
-    let newNum = Math.floor(Math.random() * 76) + 1;
-    while (playerNumberArray.includes(newNum)) {
-      newNum = Math.floor(Math.random() * 76) + 1;
-    }
-    playerNumberArray.push(newNum);
+// Generate new unique number
+const generateUniqueNumber = function (array) {
+  let randomNumber = Math.floor(Math.random() * 76 + 1);
+  while (array.includes(randomNumber)) {
+    randomNumber = Math.floor(Math.random() * 76 + 1);
   }
-
-  for (let number = 0; number <= 23; number++) {
-    let newSquare1 = document.createElement("div");
-    newSquare1.innerText = playerNumberArray[number];
-    newSquare1.classList.add("new-player-square");
-    // newSquare.classList.add("selected");
-    // newSquare.onclick = selectDay;
-    newPlayer.appendChild(newSquare1);
-  }
-};
-
-const generateBingoNumber = function (num) {
-  let randomNumber = Math.floor(Math.random() * num + 1);
-  while (alreadySelected.includes(randomNumber)) {
-    randomNumber = Math.floor(Math.random() * num + 1);
-  }
+  array.push(randomNumber);
   return randomNumber;
 };
 
-const generateNewNumber = function (num) {
-  let bingoNumber = generateBingoNumber(num);
+// Generate and mark off card
+const generateNewBingoNumber = function () {
+  let bingoNumber = generateUniqueNumber(alreadySelected);
   for (let square of gameBoardSquares) {
     if (bingoNumber == Number(square.innerText)) {
       square.classList.add("selected");
       console.log(bingoNumber);
-      alreadySelected.push(bingoNumber);
       console.log(alreadySelected);
     }
   }
 };
 
-// const markOffNumber
+// Add new player card
+const generatePlayerBoard = function () {
+  // Create the div
+  let newPlayer = document.createElement("div");
+  newPlayer.classList.add("new-player");
+  userSection.appendChild(newPlayer);
+  // Create array of 24 unique numbers
+  let playerNumberArray = [];
+  for (let num = 1; num <= 24; num++) {
+    generateUniqueNumber(playerNumberArray);
+  }
+  // Add unique numbers the div as squares
+  for (let number = 0; number <= 23; number++) {
+    let newSquare1 = document.createElement("div");
+    newSquare1.innerText = playerNumberArray[number];
+    newSquare1.classList.add("new-player-square");
+    newPlayer.appendChild(newSquare1);
+  }
+};
